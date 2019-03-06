4724f62ce55b7902e159092f0e3eb549be727b7b
Shashank bajpai <48257458+siebelexample@users.noreply.github.com>
2019-03-06 11:53:28 +0530

    Add files via upload

---
+++ NewFormAppletPR.js
@@ -0,0 +1,80 @@
+//Regenerate using:https://duncanford.github.io/prpm-code-generator/?prpm=PR&object=DesktopForm&name=NewFormApplet&userprops=&comments=Yes&logging=No
+if (typeof(SiebelAppFacade.NewFormAppletPR) === "undefined") {
+
+ SiebelJS.Namespace("SiebelAppFacade.NewFormAppletPR");
+ define("siebel/custom/NewFormAppletPR", [],
+  function () {
+   SiebelAppFacade.NewFormAppletPR = (function () {
+
+    function NewFormAppletPR(pm) {
+     SiebelAppFacade.NewFormAppletPR.superclass.constructor.apply(this, arguments);
+    }
+
+    SiebelJS.Extend(NewFormAppletPR, SiebelAppFacade.PhysicalRenderer);
+
+    NewFormAppletPR.prototype.Init = function () {
+     // Init is called each time the object is initialised.
+     // Add code here that should happen before default processing
+     SiebelAppFacade.NewFormAppletPR.superclass.Init.apply(this, arguments);
+     // Add code here that should happen after default processing
+	 
+	 this.AttachPMBinding("isAmountSet", validateAmount);
+    }
+	
+	
+	function validateAmount() {
+
+               
+                var controls = this.GetPM().Get( "GetControls" );
+                var cntrl = controls[ "Amount" ];   
+                var amountcntrl  = cntrl.GetInputName();
+                var amount = $('input[name="'+amountcntrl+'"]').val();
+                
+                if(amount == "") 
+                    return true;
+                alert("going to validate");                
+                if( amount < 10) {
+                    alert("Please enter a valid amount");
+                    return false;
+                } 
+                else {
+                    alert("This is a valid amount");
+                    return true;
+                }
+            }
+
+    NewFormAppletPR.prototype.ShowUI = function () {
+     // ShowUI is called when the object is initially laid out.
+     // Add code here that should happen before default processing
+     SiebelAppFacade.NewFormAppletPR.superclass.ShowUI.apply(this, arguments);
+     // Add code here that should happen after default processing
+    }
+
+    NewFormAppletPR.prototype.BindData = function (bRefresh) {
+     // BindData is called each time the data set changes.
+     // This is where you'll bind that data to user interface elements you might have created in ShowUI
+     // Add code here that should happen before default processing
+     SiebelAppFacade.NewFormAppletPR.superclass.BindData.apply(this, arguments);
+     // Add code here that should happen after default processing
+    }
+
+    NewFormAppletPR.prototype.BindEvents = function () {
+     // BindEvents is where we add UI event processing.
+     // Add code here that should happen before default processing
+     SiebelAppFacade.NewFormAppletPR.superclass.BindEvents.apply(this, arguments);
+     // Add code here that should happen after default processing
+    }
+
+    NewFormAppletPR.prototype.EndLife = function () {
+     // EndLife is where we perform any required cleanup.
+     // Add code here that should happen before default processing
+     SiebelAppFacade.NewFormAppletPR.superclass.EndLife.apply(this, arguments);
+     // Add code here that should happen after default processing
+    }
+
+    return NewFormAppletPR;
+   }()
+  );
+  return "SiebelAppFacade.NewFormAppletPR";
+ })
+}
